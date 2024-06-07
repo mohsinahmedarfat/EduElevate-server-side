e61b1e04cd93961e8168e93d8e3d240c76b06177
rfaraf <mohsinarfat53@gamil.com>
2024-06-07 20:45:28 +0600

    delete a certain class

--- index.js
+++ index.js
@@ -63,12 +63,12 @@ async function run() {
     });
 
     // delete a certain class
-    // app.delete("/classes/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await classCollection.deleteOne(query);
-    //   res.send(result);
-    // });
+    app.delete("/classes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await classCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
